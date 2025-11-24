be9dd425c10b6428f303ff95346b75c5416fb9f3
winfred kagendo <47690699+winniekagz@users.noreply.github.com>
2025-11-24 22:00:58 +0300

    Time: 15 ms (84.80%) | Memory: 58.8 MB (32.92%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -5,20 +5,21 @@ var characterReplacement = function(s, k) {
     let maxLength = 0;
 
     for (let i = 0; i < s.length; i++) {
+        // add unique items in hash
         if (!hash[s[i]]) {
             hash[s[i]] = 0;
         }
         hash[s[i]]++;
-
+//  maximum  repeating character
         maximumRepeating = Math.max(maximumRepeating, hash[s[i]]);
 
         let templength = i - windowStart + 1;
-
+// check length valid
         if (templength - maximumRepeating > k) {
             hash[s[windowStart]]--;
             windowStart++;
         }
-
+// return maximum repeating character
         maxLength = Math.max(maxLength, i - windowStart + 1);
     }
 
